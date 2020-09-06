2f02aa8014b0f7ecf677d112a25d757c0937c430
Cls <myjavascript123@gmail.com>
2020-09-06 21:24:00 +0530

    Updated the db url

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./containers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-pointy-86264',
-      user : 'postgres',
-      password : 'nagra123',
-      database : '\'smart-brain\''
+      host : 'process.env.DATABASE_URL',
+      ssl : 'true',
     }
   });
 
